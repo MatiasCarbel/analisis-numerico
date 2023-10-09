b06f9d8c622e35a258fa2857dd62718c65e3288e
FrancoCasavecchia <128827804+FrancCasavecchiaUCC@users.noreply.github.com>
2023-10-09 09:04:24 -0300

    Arreglado el output de sipson y trapecios , comentado para el uso

--- SegundoParcial/simpson.py
+++ SegundoParcial/simpson.py
@@ -1,6 +1,38 @@
 from sympy import*
+from tabulate import tabulate
 
-f = ""
+"""
+f = ingreso de la funcion (ej: 8 + 5 * cos(x))
+a = parametro inicial de la integral
+b = parametro final de la integral
+m = numero de particiones(en los ejercicios se lo muestra generalmente con "n")
+
+Resultado
+Imprime una tabla con los resultados de cada paso y el valor de la integral
+"""
+
+
+def simpson(funcion, a, b, m):
+    h = (b - a) / float(m)
+    s = 0
+    x = a
+
+    results = []
+
+    for i in range(1, m ):
+        n = x + (i * h)
+        n_evaluado = funcion.evalf(subs={"x": n})
+        s = s + 2 * (i % 2 + 1) * n_evaluado
+
+        a_evaluado = funcion.evalf(subs={"x": a})
+        b_evaluado = funcion.evalf(subs={"x": b})
+
+        resul = h / 3 * (a_evaluado + s + b_evaluado)
+
+        results.append([i, resul])
+
+    return results
+
 print ("Super calculo de integrales simples con el metodo de simpson c:")
 
 f = input("Ingrese su funcion en terminos de x:\n")
@@ -10,20 +42,6 @@ a = float(input("Ingrese su parametro inicial:\n"))
 b = float(input("Ingrese su parametro final:\n"))
 m = int(input("Ingrese el numero de particiones:\n"))
 
-def simpson(funcion,a,b,m):
-  h = (b-a)/float(m)
-  s = 0
-  x = a
 
-  for i in range(1,m):
-    n = x + (i*h)
-    n_evaluado = funcion.evalf(subs = {"x" : n})
-    s = s + 2*(i%2+1)*n_evaluado
-
-  a_evaluado = funcion.evalf(subs = {"x" : a})
-  b_evaluado = funcion.evalf(subs = {"x" : b})
-
-  resul = h/3 * (a_evaluado + s + b_evaluado)
-  return resul
-
-print (simpson(funcion,a,b,m))
+results = simpson(funcion, a, b, m)
+print(tabulate(results, headers=["Pasos", "Valor Integral"], tablefmt="fancy_grid"))
--- SegundoParcial/trapecio.py
+++ SegundoParcial/trapecio.py
@@ -1,13 +1,16 @@
 from sympy import sympify
+from tabulate import tabulate
 
-print("Super calculo de integrales simples con metodo del trapecio c:")
+"""
+f = ingreso de la funcion (ej: 8 + 5 * cos(x))
+a = parametro inicial de la integral
+b = parametro final de la integral
+m = numero de particiones(en los ejercicios se lo muestra generalmente con "n")
 
-f = input("Ingrese su funcion en terminos de x:\n")
-funcion = sympify(f)
+Resultado
+Imprime una tabla con los resultados de cada paso y el valor de la integral
+"""
 
-a = float(input("Ingrese su parametro inicial:\n"))
-b = float(input("Ingrese su parametro final:\n"))
-m = int(input("Ingrese el numero de particiones:\n"))  # Convierte a entero
 
 def trapecios(funcion, a, b, m):
     h = (b - a) / float(m)
@@ -16,15 +19,30 @@ def trapecios(funcion, a, b, m):
     a_evaluado = 0
     b_evaluado = 0
 
-    for i in range(1, m):
+    results = []
+
+    for i in range(1, m ):
         n = a + (i * h)
         n_evaluado = funcion.evalf(subs={"x": n})
         s = s + n_evaluado
 
-    a_evaluado = funcion.evalf(subs={"x": a})
-    b_evaluado = funcion.evalf(subs={"x": b})
-    resul = h / 2 * (a_evaluado + 2 * s + b_evaluado)
+        a_evaluado = funcion.evalf(subs={"x": a})
+        b_evaluado = funcion.evalf(subs={"x": b})
+        resul = h / 2 * (a_evaluado + 2 * s + b_evaluado)
 
-    return resul
+        results.append([i, resul])
 
-print(trapecios(funcion, a, b, m))
+    return results
+
+print("Super calculo de integrales simples con metodo del trapecio c:")
+
+f = input("Ingrese su funcion en terminos de x:\n")
+funcion = sympify(f)
+
+a = float(input("Ingrese su parametro inicial:\n"))
+b = float(input("Ingrese su parametro final:\n"))
+m = int(input("Ingrese el numero de particiones:\n"))  # Convierte a entero el numero de particiones
+
+results = trapecios(funcion, a, b, m)
+print(tabulate(results, headers=["Pasos", "Valor Integral"], tablefmt="fancy_grid"))
+
