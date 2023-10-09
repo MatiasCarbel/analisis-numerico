0ff3cc5d4e2451f85a41a2bcae200a6c8016fe1f
FrancoCasavecchia <128827804+FrancCasavecchiaUCC@users.noreply.github.com>
2023-10-09 08:26:41 -0300

    Arreglado metodo de euler

--- SegundoParcial/metodoEuler.py
+++ SegundoParcial/metodoEuler.py
@@ -9,22 +9,17 @@ h = 0.1
 x = np.arange(0, 4 + h, h)
 y0 = 2  # Condición inicial
 
-# Método de Euler explícito
+# Euler's method
 y = np.zeros(len(x))
 y[0] = y0
 
+# Output header
+print("---RESULTS---")
+print("X\t\tY")
+
 for i in range(0, len(x) - 1):
     y[i + 1] = y[i] + h * f(x[i], y[i])
+    print(f"{x[i]:.2f}\t\t{y[i]:.4f}")
 
-# Cálculo de la ecuación exacta
-exact_solution = np.exp(0.8 * x) - 0.5 * y
-
-plt.figure(figsize=(12, 8))
-plt.plot(x, y, 'bo--', label='Aproximada')
-plt.plot(x, exact_solution, 'g', label='Exacta')  # Corregir el uso de x en lugar de y0
-plt.title('Solución Aproximada y Exacta para Ecuación Diferencial')
-plt.xlabel('x')
-plt.ylabel('y(x)')
-plt.grid()
-plt.legend(loc='lower right')
-plt.show()
+# Print the last point
+print(f"{x[-1]:.2f}\t\t{y[-1]:.4f}")
