7882675e29a4ee1fa4ae2c8f214f28aec54e79a5
FrancoCasavecchia <128827804+FrancCasavecchiaUCC@users.noreply.github.com>
2023-10-09 11:19:13 -0300

    Runge-Kutta ambos metodos terminado y comentados

--- SegundoParcial/metodoRunge-Kuppa2.py
+++ SegundoParcial/metodoRunge-Kuppa2.py
@@ -1,6 +1,15 @@
 import numpy as np
 from tabulate import tabulate
 
+"""
+# F = ODE escrita en forma estándar.(Cada vez que se quiera usar un exponente es necesitaro np.exp(x) al principio)
+# h = Tamaño del paso
+# x = Vector de valores de x (Las primeras dos posiciónes son los valores iniciales de x, osea el intervalo ej [0,4])
+# y0 = Condición inicial de y osea y(0) = 1, hace referencia al valor de y en la primera posición de x, y es lo que aproximas
+
+El resultado va a ser mostrado en una lsita por interaciones, y al final se imprime el último valor de x y y
+"""
+
 # Función para el método de Runge-Kutta de segundo orden
 def runge_kutta_second_order(f, x, y0, h):
     results = []
---
+++ SegundoParcial/metodoRunge-Kutta4.py
@@ -0,0 +1,43 @@
+import numpy as np
+from tabulate import tabulate
+
+"""
+# F = ODE escrita en forma estándar.(Cada vez que se quiera usar un exponente es necesitaro np.exp(x) al principio)
+# h = Tamaño del paso
+# x = Vector de valores de x (Las primeras dos posiciónes son los valores iniciales de x, osea el intervalo ej [0,4])
+# y0 = Condición inicial de y osea y(0) = 1, hace referencia al valor de y en la primera posición de x, y es lo que aproximas
+
+
+El resultado va a ser mostrado en una lsita por interaciones, y al final se imprime el último valor de x y y
+
+"""
+
+# Definición de la ecuación diferencial y los parámetros
+f = lambda x, y: np.exp(0.8 * x) - 0.5 * y
+h = 0.1  # Tamaño del paso
+x = np.arange(0, 4 + h, h)
+y0 = 2  # Condición inicial
+
+# Método de Runge-Kutta de cuarto orden
+y = np.zeros(len(x))
+y[0] = y0
+
+# Almacenar resultados en una lista
+results = []
+
+# Iteración para calcular resultados
+for i in range(0, len(x) - 1):
+    k1 = h * f(x[i], y[i])
+    k2 = h * f(x[i] + h/2, y[i] + k1/2)
+    k3 = h * f(x[i] + h/2, y[i] + k2/2)
+    k4 = h * f(x[i] + h, y[i] + k3)
+    
+    y[i + 1] = y[i] + (k1 + 2*k2 + 2*k3 + k4) / 6
+    results.append([x[i], y[i]])
+
+# Agregar el último punto
+results.append([x[-1], y[-1]])
+
+# Mostrar los resultados en formato de tabla
+table_headers = ["X", "Y"]
+print(tabulate(results, headers=table_headers, tablefmt="grid"))
