d4507c782c838f6b60566a06d159e2af5ce0bc3c
FrancoCasavecchia <128827804+FrancCasavecchiaUCC@users.noreply.github.com>
2023-10-09 08:39:37 -0300

    MetodoHeun Terminado

--- SegundoParcial/metodoHeun.py
+++ SegundoParcial/metodoHeun.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 # F = ODE escrita en forma estándar.(Cada vez que se quiera usar un exponente es necesitaro np.exp(x) al principio)
 # h = Tamaño del paso
@@ -10,15 +8,14 @@ import matplotlib.pyplot as plt
 #Resultado: Imprime una tabla con los valores de x y y en cada iteración, y al final imprime el último valor de x y y
 #El ultimo valor de y es el resultado final de la aproximación
 
-plt.style.use('seaborn-poster')
 
-# Definición de la ecuación diferencial y los parámetros
+# Define the differential equation and parameters
 f = lambda x, y: np.exp(0.8 * x) - 0.5 * y
-h = 0.1  # Tamaño del paso
-x = np.arange(0, 4 + h, h)
-y0 = 2  # Condición inicial
+h = 0.1  # Step size
+x = np.arange(0, 4 + h, h)  # Adjust the range to match your desired output
+y0 = 2  # Initial condition
 
-# Euler's method
+# Improved Euler Method (Heun's Method)
 y = np.zeros(len(x))
 y[0] = y0
 
@@ -26,8 +23,15 @@ y[0] = y0
 print("---RESULTS---")
 print("X\t\tY")
 
+# Iterate over the range of x values
 for i in range(0, len(x) - 1):
-    y[i + 1] = y[i] + h * f(x[i], y[i])
+    # Predictor Step
+    y_predictor = y[i] + h * f(x[i], y[i])
+    
+    # Corrector Step
+    y[i + 1] = y[i] + 0.5 * h * (f(x[i], y[i]) + f(x[i + 1], y_predictor))
+    
+    # Print the X and Y values for each step
     print(f"{x[i]:.2f}\t\t{y[i]:.4f}")
 
 # Print the last point
