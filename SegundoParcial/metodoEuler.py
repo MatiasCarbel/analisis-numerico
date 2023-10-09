2702b05a7b6f6e8ceb5f98f651e2387b608e3a20
FrancoCasavecchia <128827804+FrancCasavecchiaUCC@users.noreply.github.com>
2023-10-09 08:36:18 -0300

    Comenttado

--- SegundoParcial/metodoEuler.py
+++ SegundoParcial/metodoEuler.py
@@ -1,13 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# F = ODE escrita en forma estándar.(Cada vez que se quiera usar un exponente es necesitaro np.exp(x) al principio)
+# h = Tamaño del paso
+# x = Vector de valores de x (Las primeras dos posiciónes son los valores iniciales de x, osea el intervalo ej [0,4])
+# y0 = Condición inicial de y osea y(0) = 1, hace referencia al valor de y en la primera posición de x, y es lo que aproximas
+
+#Resultado: Imprime una tabla con los valores de x y y en cada iteración, y al final imprime el último valor de x y y
+#El ultimo valor de y es el resultado final de la aproximación
+
 plt.style.use('seaborn-poster')
 
 # Definición de la ecuación diferencial y los parámetros
-f = lambda x, y: np.exp(0.8 * x) - 0.5 * y
-h = 0.1
+f = lambda x, y: 2 * x * np.exp(3) + 12 * x * np.exp(2) - 20 * x + 8.5
+h = 0.5
 x = np.arange(0, 4 + h, h)
-y0 = 2  # Condición inicial
+y0 = 1  # Condición inicial
 
 # Euler's method
 y = np.zeros(len(x))
